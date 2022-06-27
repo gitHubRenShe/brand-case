52aff0529df0190018815c88b05634be3a648760
renshe <1912440145@qq.com>
2022-06-27 18:11:37 +0800

    first commit

---
+++ src/main/java/com/itheima/mapper/BrandMapper.java
@@ -0,0 +1,66 @@
+package com.itheima.mapper;
+
+import com.itheima.pojo.Brand;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+public interface BrandMapper {
+    /**
+     * 查询所有
+     * @return
+     */
+    @Select("select * from db1.tb_brand")
+    @ResultMap("brandResultMap")
+    List<Brand> selectAll();
+
+    /**
+     * 添加数据
+     * @param brand
+     */
+    @Insert("insert into db1.tb_brand values(null,#{brandName},#{companyName},#{ordered},#{description},#{status})")
+    void add(Brand brand);
+
+    /**
+     * 修改
+     * @param brand
+     */
+    @Update("update db1.tb_brand set brand_name = #{brandName},company_name = #{companyName},ordered = #{ordered},description=#{description},status=#{status} where id = #{id}")
+    void update(Brand brand);
+    /**
+     * 批量删除
+     * @param ids
+     */
+    void deleteByIds(@Param("ids") int[] ids);
+
+    /**
+     * 分页查询
+     * @param begin
+     * @param size
+     * @return
+     */
+    @Select("select * from db1.tb_brand limit #{begin},#{size}")
+    List<Brand> selectByPage(@Param("begin") int begin,@Param("size") int size);
+
+    /**
+     * 查询总记录条数
+     * @return
+     */
+    @Select("select count(*) from db1.tb_brand")
+    int selectTotalCount();
+
+    /**
+     * 分页条件查询
+     * @param begin
+     * @param size
+     * @return
+     */
+    List<Brand> selectByPageAndCondition(@Param("begin") int begin,@Param("size") int size,@Param("brand") Brand brand);
+
+    /**
+     * 查询总记录条数
+     * @return
+     */
+    int selectTotalCountByCondition(Brand brand);
+
+}
